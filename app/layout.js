c08abaa5480d2ec55ba62cb7b4766b371e82665a
KloKlo <67327292+FlorentBL@users.noreply.github.com>
2025-07-30 19:12:36 +0000

    go

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import "./globals.css";
-import Navbar from "../components/Navbar"; // adapte le chemin si besoin
+import Navbar from "../components/Navbar";
 import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
@@ -12,13 +12,13 @@ export default function RootLayout({ children }) {
     <html lang="fr">
       <head>
         <link rel="icon" href="/favicon.png" type="image/png" />
-        {/* Mets ici d'autres balises meta si besoin */}
       </head>
       <body>
         <Navbar />
         <div style={{ paddingTop: 60 }}>
           {children}
         </div>
+        <Analytics /> {/* <-- Ajoute cette ligne ici */}
       </body>
     </html>
   );
